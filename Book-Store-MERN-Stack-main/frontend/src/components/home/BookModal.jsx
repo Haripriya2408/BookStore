126c26d485d7d4f01cb2ca034b65baa6ce52f425
mathisri15 <srimathis3015@gmail.com>
2023-11-30 19:18:20 +0530

    rc/components/home/BookModal.jsx

--- Book-Store-MERN-Stack-main/frontend/src/components/home/BookModal.jsx
+++ Book-Store-MERN-Stack-main/frontend/src/components/home/BookModal.jsx
@@ -1,48 +1,49 @@
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
 import { AiOutlineClose } from 'react-icons/ai';
 import { PiBookOpenTextLight } from 'react-icons/pi';
 import { BiUserCircle } from 'react-icons/bi';
-
+import { useState } from 'react';
 const BookModal = ({ book, onClose }) => {
   const [searchTerm, setSearchTerm] = useState('');
-
   return (
     <>
-      <input
-        type="text"
-        placeholder="Search by title"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="my-4 p-2 mr-2 border border-slate-600 rounded-md float-right"
-      />
+    <input
+    type="text"
+    placeholder="Search by title"
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+    className="my-4 p-2 mr-2 border border-slate-600 rounded-md float-right"
+  />
+    <div
+      className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
+      onClick={onClose}
+    >
       <div
-        className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
-        onClick={onClose}
+        onClick={(event) => event.stopPropagation()}
+        className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'
       >
-        <div
-          onClick={(event) => event.stopPropagation()}
-          className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'
-        >
-          <AiOutlineClose
-            className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
-            onClick={onClose}
-          />
-          <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>
-            {book.publishYear}
-          </h2>
-          <h4 className='my-2 text-gray-500'>{book._id}</h4>
-          <div className='flex justify-start items-center gap-x-2'>
-            <PiBookOpenTextLight className='text-red-300 text-2xl' />
-            <h2 className='my-1'>{book.title}</h2>
-          </div>
-          <div className='flex justify-start items-center gap-x-2'>
-            <BiUserCircle className='text-red-300 text-2xl' />
-            <h2 className='my-1'>{book.author}</h2>
-          </div>
-          <p className='mt-4'>Anything You want to show</p>
-          <p className='my-2'>{book.description}</p>
+        <AiOutlineClose
+          className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
+          onClick={onClose}
+        />
+        <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>
+          {book.publishYear}
+        </h2>
+        <h4 className='my-2 text-gray-500'>{book._id}</h4>
+        <div className='flex justify-start items-center gap-x-2'>
+          <PiBookOpenTextLight className='text-red-300 text-2xl' />
+          <h2 className='my-1'>{book.title}</h2>
         </div>
+        <div className='flex justify-start items-center gap-x-2'>
+          <BiUserCircle className='text-red-300 text-2xl' />
+          <h2 className='my-1'>{book.author}</h2>
+        </div>
+        <p className='mt-4'>About the Book</p>
+        <p className='my-2'>
+         {book.desription}
+        </p>
       </div>
+    </div>
     </>
   );
 };
